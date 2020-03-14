36e90f19cbb15d09c8d591b7f6b7ebaaf01afa09
Terminal-Est <cstephen1982@gmail.com>
2020-03-14 16:38:57 +1100

    Added player death
    Added player death. When player drops off level, the collider in the pet will transport the player back to the starting position.

---
+++ BIT2DUnityProject/Assets/Scripts/Death.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+
+{
+
+    bool death;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        death = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        if (death)
+        {
+            player.transform.position = new Vector3(-2.089f, 0.046f, 0.0f);
+            death = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            death = true;
+            print("dead");
+        }
+    }
+}
