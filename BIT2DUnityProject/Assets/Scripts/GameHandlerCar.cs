9683858bf3b8e993c4caff81abd12c8a9fc155d9
Terminal-Est <cstephen1982@gmail.com>
2020-03-17 18:18:43 +1100

    Fixed camera bug on Top Down

---
+++ BIT2DUnityProject/Assets/Scripts/GameHandlerCar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandlerCar : MonoBehaviour
+{
+
+    public CameraFollowCar cameraFollow;
+    public Transform playerTransform;
+
+    void Start()
+    {
+        cameraFollow.Setup(() => playerTransform.position);
+    }
+
+}
