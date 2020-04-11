87bf4ed4d65e8e5322bc88d6f3376134ab3c2492
Terminal-Est <cstephen1982@gmail.com>
2020-04-11 17:42:13 +1000

    Added car UI elements

---
+++ BIT2DUnityProject/Assets/Scripts/CarUIScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CarUIScript : MonoBehaviour
+{
+
+    private float damage;
+    private float fuel;
+    private float money;
+
+    Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (text.tag == "CarFuel")
+        {
+            fuel = PlayerStats.fuel;
+            text.text = "Fuel: " + Mathf.Round(fuel);
+        } else if (text.tag == "CarDmg")
+        {
+            damage = PlayerStats.vehicleDamage;
+            text.text = "Damage: " + Mathf.Round(damage);
+        } else if (text.tag == "Cash")
+        {
+            money = PlayerStats.cash;
+            text.text = "Cash: " + Mathf.Round(money);
+        }
+    }
+
+}
